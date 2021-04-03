a279704c162e54d996c6739a55c4aeb77375854d
JosiasAurel <ndjosiasaurel@gmail.com>
2021-04-03 17:28:32 +0100

    make app fetch links and display in a beautiful way

--- components/Link.jsx
+++ components/Link.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "../styles/index.module.css";
 
 const Link = ({ title, link, description }) => {
     return (
-        <div>
+        <div className={styles.link}>
             <h2>{title}</h2>
             <a about="blank" href={link}>{link}</a>
             <p>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "../components/Link";
 import styles from "../styles/index.module.css";
 
 // import utility functions
+// import { getLinks } from "../utils/fetchRequest";
 
 const Index = () => {
+    const [Links, setLinks] = useState([]);
+
+    /* const getAndSetLinks = async () => {
+
+        let results = await getLinks("http://localhost:3000/api/getlinks");
+        setLinks(results);
+    } */
+
+    // getAndSetLinks();
+
+    useEffect(() => {
+        //we better handle getting data here so it can be relative to the host
+        const getLinks = async () => {
+            const res = await fetch("/api/getlinks");
+            const data = await res.json();
+            return data;
+        }
+
+        const getAndSetLinks = async () => {
+            let links = await getLinks();
+            setLinks(links);
+        }
+
+        getAndSetLinks()
+
+    }, [Links])
     return (
-        <>
+        <div className={styles.page}>
             <header>
-                <h2>LinksBook</h2>
+                <h1>LinksBook</h1>
             </header>
 
-            <main className={styles.link}>
+            <h2>Here are your links</h2>
+            <main className={styles.links}>
+                {Links.map(({title, description, link}) => {
+                    return (
+                        <Link 
+                        title={title}
+                        description={description}
+                        link={link}
+                        />
+                    )
+                })}
             </main>
-        </>
+
+            <div>
+                <button>
+                    
+                </button>
+            </div>
+        </div>
     )
 };
 
