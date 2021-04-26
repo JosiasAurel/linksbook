7600143cf40609d228238c1132c6c088d0ae2d25
JosiasAurel <ndjosiasaurel@gmail.com>
2021-04-26 04:50:27 +0100

    add auto gen OG image

--- client/pages/public/[collection].jsx
+++ client/pages/public/[collection].jsx
@@ -4,12 +4,15 @@ import LinkCard from "../../components/publicLink";
 
 import styles from "../../styles/dash.module.css";
 
+import Head from "next/head";
+
 const PublicCollection = (props) => {
     let collection = props.pageProps.id.collection;
 
     console.log(collection);
 
     const [Coll, setColl] = useState([]);
+    const [lk, setLk] = useState({});
 
     function fetchAndSetLinksBooks() {
         fetch(`https://linksbook-server.vercel.app/getlinks/${collection}`)
@@ -19,12 +22,39 @@ const PublicCollection = (props) => {
         })
     }
 
+    function getLinksBookData() {
+        fetch(`https://linksbook-server.vercel.app/linksbookdata/${collection}`)
+            .then(res => res.json())
+            .then(data => {
+                setLk(data);
+            })
+    }
+
     useEffect(() => {
         fetchAndSetLinksBooks();
+        getLinksBookData();
     }, [])
 
     return (
         <div className={styles.linksPage}>
+            <Head>
+<title>LinksBook - {lk.title}</title>
+<meta name="title" content={lk.title} />
+<meta name="description" content={lk.description} />
+
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://linksbook.me/" />
+<meta property="og:title" content={lk.title} />
+<meta property="og:description" content={lk.description} />
+<meta property="og:image" content={`https://og-image-delta-one.vercel.app/${(lk.title).split(" ").join("%20")}.png`} />
+
+
+<meta property="twitter:card" content="summary_large_image" />
+<meta property="twitter:url" content="https://linksbook.me/" />
+<meta property="twitter:title" content={lk.title} />
+<meta property="twitter:description" content={lk.description} />
+<meta property="twitter:image" content={`https://og-image-delta-one.vercel.app/${(lk.title).split(" ").join("%20")}.png`}></meta>
+            </Head>
             <main className={styles.links}>
                 {Coll === [] ? <h2> No Links in this collection </h2>
                 : Coll.map(link => {
