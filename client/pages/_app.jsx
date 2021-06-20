66ecc3ef00281bf4ba4558eb3bba2deb85356ce0
Josias Aurel <ndjosiasaurel@gmail.com>
2021-06-20 04:55:42 +0100

    add jost analytics

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -5,7 +5,12 @@ import Head from "next/head";
 
 const App = ({Component, ...pageProps}) => {
     return (
-                <Component {...pageProps} />
+        <div>
+            <Head>
+                <script src="https://cdn.jsdelivr.net/npm/jost-analytics@1.3.0/jost.min.js"></script>
+            </Head>
+           <Component {...pageProps} />
+        </div>
     )
 }
 
