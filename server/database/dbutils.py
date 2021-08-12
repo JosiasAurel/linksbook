e58025421633bbd011b1e2321f0c79b40edb050a
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-12 12:19:14 +0100

    create function to generate IDs

--- server/database/dbutils.py
+++ server/database/dbutils.py
@@ -1,6 +1,12 @@
 from datetime import datetime
+import secrets
 
 
 def get_date() -> str:
     date = str(datetime.utcnow()).split(" ")[0]
     return date
+
+
+def gen_id() -> str:
+    id_ = secrets.token_hex(10)
+    return id_
