d6ea674f8bf65d3a8c66c8ec52d0c21e9a6ec8cd
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-12 12:19:30 +0100

    create function to create collection

--- server/database/collections.py
+++ server/database/collections.py
@@ -1,6 +1,6 @@
 from deta import Base
 from index import db
-from dbutils import get_date
+from dbutils import get_date, gen_id
 from pydantic import BaseModel
 from datetime import datetime
 import dateparser
@@ -14,8 +14,32 @@ class Collection(BaseModel):
     date: str
 
 
-def create_collection(title, image):
-    return
+def create_collection(title, image: str = None):
+
+    state = {"status": False}  # default is false
+
+    if image is not None:
+        new_collection = {
+            "title": title,
+            "image": image,
+            "date": get_date(),
+            "key": gen_id()
+        }
+
+        # save new collection to the database
+        db.put(new_collection)
+    else:
+        new_collection = {
+            "title": title,
+            "image": image,  # replace with an image link,
+            "date": get_date(),
+            "key": gen_id()
+        }
+
+        # save new collection to the database
+        db.put(new_collection)
+
+    return state
 
 
 print(get_date())
