17bf4e248dfa5b7d8340fef8d1ec37707be303fe
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-13 14:12:18 +0100

    add global styles

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,6 +2,8 @@ import React, { FunctionComponent } from "react";
 
 import { AppProps } from "next/app";
 
+import "../styles/global.css";
+
 const LinksBookApp: FunctionComponent<AppProps> = ({ Component, pageProps }): JSX.Element => {
     return <Component {...pageProps} />;
 }
