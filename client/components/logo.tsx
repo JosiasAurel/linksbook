05ff0362c37941744b456f73a9dfc21a8b2fd587
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-21 07:10:22 +0100

    update header

---
+++ client/components/logo.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from "react";
+
+import styles from "../styles/components.module.css";
+
+const Logo: FunctionComponent = () => {
+    return (
+        <div className={styles.logo}>
+            <h2>L</h2>
+        </div>
+    )
+}
+
+export default Logo;
