950a9e22744cb415fd17e143f9f2ead23ab03d36
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-21 07:49:48 +0100

    create route for creating collection

--- server/main.py
+++ server/main.py
@@ -1,4 +1,6 @@
+import collections
 from database.user import create_user
+from database.collections import create_collection
 from fastapi import FastAPI, Request
 from deta import Deta
 from fastapi.middleware.cors import CORSMiddleware
@@ -25,3 +27,11 @@ async def _save_user(request: Request):
     status = create_user(
         user_data["name"], user_data["email"], user_data["uid"])
     return status
+
+
+@app.post("/create-collection")
+async def _create_collection(request: Request):
+    collection_data = await request.json()  # extract collection props
+    new_collection = create_collection(
+        collection_data["owner"], collection_data["title"], collection_data["description"])
+    return new_collection
