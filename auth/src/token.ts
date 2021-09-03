7ae941f15dba92ff950531f812332745019e52fa
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-03 19:38:31 +0100

    add mock function for verifyIng and validating token

--- auth/src/token.ts
+++ auth/src/token.ts
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken";
+const jwt = require("jsonwebtoken");
 
 const SECRET: string = "yolo";
 
@@ -17,3 +17,31 @@ function createAuthToken({email, userID}: AuthTokenCredentials): string {
     return newToken;
 
 }
+
+function verifyToken(token: string): any {
+    const verifiedToken: any = jwt.verify(token, SECRET, {
+        issuer: "linksbook-v2-ww"
+    });
+
+    console.log(verifiedToken);
+
+    return;
+}
+
+// Validate token
+/*  
+    We validate token by taking the difference between issuing date and expiring date
+    Then we get the timespan of expiresIn (30 days) in milliseconds.
+    If the difference above is greater or equals the value of 30d in ms
+    Then the token is valid... Same applies to issuer having to be linksbook-v2-ww
+
+*/
+function validateToken(token: string): any {
+    const validatedToken: any = jwt.verify(token, SECRET, {
+        issuer: "linksbook-v2-ww"
+    });
+}
+
+let token = createAuthToken({email: "josias@josiasw.dev", userID: "23434"});
+console.log(token);
+verifyToken(token);
