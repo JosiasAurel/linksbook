a6309cc32de01c6f424ace5f9ea58632f9ab23fb
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-04 12:09:25 +0100

    create query and mutation methods

--- server/graphql/typedefs.ts
+++ server/graphql/typedefs.ts
@@ -6,25 +6,40 @@ const typeDefinitions: any = gql`
 type Link {
     annotation: String
     tags: [String]
-    note: Note
-    url: String
-    owner: String
-    id: ID
-    createdAt: String
+    note: String
+    url: String!
+    owner: String!
+    id: ID!
+    createdAt: String!
 }
 
 type Group {
-    id: ID
-    links: [ID]
-    name: String
-    createdAt: String
+    id: ID!
+    links: [Link]
+    name: String!
+    createdAt: String!
 }
 
-type Note {
-    id: ID
-    link: ID
-    content: String
-    title: String
+
+type ActionStatus {
+    status: String
 }
 
+type Query {
+    getLink(linkResolver: String!): Link!
+    getGroup(groupResolver: String!): Group!
+    getLinks: [Link]!
+    getGroups: [Group]!
+    searchLinks(search: String): [Link]
+    searchGroups(search: String): [Link]
+}
+
+type Mutation {
+    createLink(annotation: String!, url: String!, tags: [String]!): ActionStatus
+    updateLink(annotation: String, url: String, tags: [String]): ActionStatus
+    deleteLink(linkId: String!): ActionStatus
+    createGroup(name: String!, tags: [String], links: [String]): ActionStatus
+    updateGroup(name: String, tags: [String], links: [String]): ActionStatus
+    deleteGroup(groupId: String): ActionStatus
+}
 `;
---
+++ server/index.d.ts
@@ -0,0 +1,27 @@
+
+// Application
+// Custom models type definitions
+
+declare interface Link {
+    annotation: string
+    tags: Array<string>
+    note: string
+    url: string
+    owner: string
+    id: string
+    createdAt: string
+}
+
+declare interface Group {
+    id: string
+    links: Array<string>
+    name: string
+    createdAt: string
+}
+
+declare interface Note {
+    id: string
+    link: string
+    content: string
+    title: string
+}
