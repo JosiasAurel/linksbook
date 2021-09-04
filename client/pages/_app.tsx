b0b449ec0be7128987a0a917d2c5dc6c419eee32
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-04 13:15:53 +0100

    add clerk as auth handler

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,14 +2,41 @@ import React, { FunctionComponent } from "react";
 
 import { AppProps } from "next/app";
 
-// the navigation context
+/* // the navigation context
 import { NavigationContext, ApplicationNavigation } from "../contexts/navigation";
+ */
+
+import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn } from "@clerk/nextjs";
+
+import { useRouter } from "next/router";
 
 const LinksBookApp: FunctionComponent<AppProps> = ({ Component, pageProps }): JSX.Element => {
+
+    // list of public pages
+    const publicPages: Array<string> = [];
+
+    // get current page pathname
+    const { pathname } = useRouter();
+
+    // is the current page a public page ?
+    const isPublicPage: boolean = publicPages.includes(pathname);
+
     return (
-        <ApplicationNavigation>
-            <Component {...pageProps} />
-        </ApplicationNavigation>
+        <ClerkProvider>
+            {isPublicPage ? (
+                <Component {...pageProps} />
+            ) : (
+                <>
+                    <SignedIn>
+                        <Component {...pageProps} />
+                    </SignedIn>
+                    <SignedOut>
+                        <RedirectToSignIn />
+                    </SignedOut>
+                </>
+            )
+            }
+        </ClerkProvider>
     )
 }
 
