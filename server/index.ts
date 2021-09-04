4c1c9a7377c200b52c3bdb0e6a830e1dcfd3a271
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-04 15:25:39 +0100

    add route for completing account creation

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,8 @@
 import express, { Application, Request, Response } from "express";
 
+// models...
+import { saveUser } from "./models/user";
+
 const port: number = 5000;
 
 // init express server app
@@ -7,6 +10,14 @@ const app: Application = express();
 
 app.get("/", (req: Request, res: Response) => {
     res.send("LinksBook server working");
+});
+
+app.post("/complete-account", async (req: Request, res: Response) => {
+    const { name, email, userId } = req.body;
+
+    const completeAccountResult = await saveUser(name, email, userId);
+
+    res.send(completeAccountResult);
 });
 
 app.listen(port, () => console.log(`Server working on port ${port}`));
