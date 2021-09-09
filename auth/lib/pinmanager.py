87afdbc80cc17c66017d3573004c640b1cd6d3e1
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-09 02:54:16 +0100

    create function to handle pins

---
+++ auth/lib/pinmanager.py
@@ -0,0 +1,20 @@
+from deta import Deta
+import secrets
+from .genid import generate_id
+
+deta = Deta("a0ojq87u_xgq3dQQLkXj3YBsJ5iJKZ5MTAtYmCLoF")
+
+pinsdb = deta.Base("pins")
+
+
+def generate_pin() -> str:
+    return secrets.token_hex(5)
+
+
+def create_pin() -> str:
+    new_pin = generate_pin()
+    try:
+        pinsdb.put({"pin": new_pin}, new_pin)
+        return {"status": "Success", "pin": new_pin}
+    except:
+        return {"status": "Failed", "type": "CreatePin"}
