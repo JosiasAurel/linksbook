083a7ee29d04b047520c1b87787180df749cbe76
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-10 02:50:27 +0100

    add functions to get user and userId by email

--- auth/models/user.py
+++ auth/models/user.py
@@ -19,3 +19,20 @@ def update_user(name: str, email: str) -> T.Dict[str, str]:
         usersdb.update({"name": name}, email)
     except:
         return {"status": "Failed", "type": "UpdateUser"}
+
+
+def get_user_by_email(email: str) -> any:
+    try:
+        users = usersdb.fetch({"email": email}).items
+        users[0]["status"] = "Success"
+        return users[0]
+    except:
+        return {"status": "Failed"}
+
+
+def get_user_id_by_email(email: str) -> str:
+    user = get_user_by_email(email)
+    if user["status"] != "Failed":
+        return user["userId"]
+    else:
+        return None
