9f747d3e7be8f2d1a05da947f2e2c86e304f817c
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-10 02:16:01 +0100

    create a function to generate token expiry

--- auth/lib/tokenmanager.py
+++ auth/lib/tokenmanager.py
@@ -1,10 +1,19 @@
 from deta import Deta
 import jwt
+import datetime
 
 deta = Deta("a0ojq87u_xgq3dQQLkXj3YBsJ5iJKZ5MTAtYmCLoF")
 
 # create or access JWT db
 tokensdb = deta.Base("tokens")
+
+
+def set_token_expiry_date_to(days: int) -> datetime:
+    current_date = datetime.datetime.utcnow()
+    add_days = datetime.timedelta(days=days)
+    new_date = current_date + add_days
+
+    return new_date
 
 
 def create_token(name, email) -> str:
