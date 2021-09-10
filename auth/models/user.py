78539fbb7af8440fba5dc1ac7c9a0de6fd7318be
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-10 02:49:37 +0100

    create models and user functions

---
+++ auth/models/user.py
@@ -0,0 +1,14 @@
+from deta import Deta
+import typing as T
+
+deta = Deta("a0ojq87u_xgq3dQQLkXj3YBsJ5iJKZ5MTAtYmCLoF")
+
+usersdb = deta.Base("users")
+
+
+def create_user(name: str, email: str) -> T.Dict[str, str]:
+    try:
+        usersdb.put({"name": name, "email": email}, key=email)
+        return {"status": "Success"}
+    except:
+        return {"status": "Failed", "type": "CreateUser"}
