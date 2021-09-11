bfd02ed9d8e3cab050ce51f89ae68a31e26bff04
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-11 18:58:13 +0100

    add function to get user by email

---
+++ server/models/user.ts
@@ -0,0 +1,12 @@
+import { deta } from "./index";
+
+const users = deta.Base("users");
+
+async function getUserByEmail(email: string): Promise<any> {
+    const users__: any = await (await users.fetch({email})).items;
+    const user = users__[0];
+
+    return user;
+}
+
+export { getUserByEmail };
