e5a3bd6b0b095fc9a945f78b1ebfb8e2dda19d17
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-11 18:59:51 +0100

    add user identification step to GraphQL requests

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,12 @@
 import express, { Application, Request, Response } from "express";
+import { ApolloServer } from "apollo-server-express";
+
+// load type definitions and resolvers
+import { typeDefinitions } from "./graphql/typedefs";
+import { resolvers } from "./graphql/resolvers";
 
 import cors from "cors";
+import { authenticateUser } from "./utils/auth";
 
 // models...
 
@@ -18,6 +24,25 @@ app.get("/", (req: Request, res: Response) => {
     res.send("LinksBook server working");
 });
 
+// mount apollo server on express application
+const apolloServer = new ApolloServer({
+    typeDefs: typeDefinitions,
+    resolvers,
+    context: ({ req }) => {
+        // handle request context
+
+        const requestHeaders: any = req.headers;
+
+        const authorization: any = requestHeaders?.authorization;
+
+        // get the JWT from the request headers
+        const authToken: string = authorization?.split(" ")[1]; 
+
+        const userInfo = authenticateUser(authToken);
+
+        return userInfo;
+    }
+})
 
 
 app.listen(port, () => console.log(`Server working on port ${port}`));
