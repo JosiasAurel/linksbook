a3aa656ca660fb5f9b5ca2656498a14b6040c2a1
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-11 15:02:31 +0100

    create resolver for link

--- server/graphql/resolvers.ts
+++ server/graphql/resolvers.ts
@@ -1,1 +1,17 @@
 
+// import model CRUD handlers
+
+// import link handlers
+import { getLink, getAllLinks, createLink, updateLink, deleteLink, searchLinks } from "../models/links";
+
+const resolvers = {
+    Query: {
+        getLink: async (parent: any, args: any, context: any) => {
+            // get the link ID from args
+            const { linkId } = args;
+            const link = await getLink(linkId);
+
+            return link;
+        }
+    }
+}
