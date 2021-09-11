54c93683960b1b67ca5d7de3363a0df67d1c1036
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-11 17:11:22 +0100

    add handler for creating a collection

--- server/models/collection.ts
+++ server/models/collection.ts
@@ -1,4 +1,18 @@
-import { deta } from "./index";
+import { deta, generateModelKey } from "./index";
 
 // collections database table
 const collections = deta.Base("collections");
+
+async function createCollection(name: string): Promise<any> {
+
+    try {
+        const newCollection = collections.put({
+            name,
+
+        }, generateModelKey());
+
+        return {status: "Success"};
+    } catch (error: any) {
+        return {status: "Failed"};
+    }
+}
