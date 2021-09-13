8be7b5abe3fe3f3e86d7ed3805e7a7a6a7108211
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-13 19:07:45 +0100

    swap content

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,45 +1,10 @@
 import React, { FunctionComponent, useState, useContext, useEffect } from "react";
 
-import styles from "../styles/index.module.css";
-
-import User from "../components/User";
-import ColorToggle from "../components/colorToggle";
-import Tag from "../components/Tag";
-import CopyLink from "../components/CopyLink";
-import Folder from "../components/Folder";
-
 const HomePage: FunctionComponent = (): JSX.Element => {
-
-    const [name, setName] = useState<string>("");
-    const handleChange: Function = (event, handler) => {
-        handler(event.target.value);
-    }
 
     return (
         <div>
-
-            <div>
-                Profile
-                <User profile="/josias.jpg" name="Josias" />
-            </div>
-            <div>
-                Tag
-                <Tag name="Hello" />
-            </div>
-
-            <div>
-                Copy Link
-                <CopyLink link="https://hello.world" />
-            </div>
-
-            <div>
-                Folders
-                ------
-                Parent Type Folder
-                <Folder type="Parent" label="Mobile" />
-                Child Type Folder
-                <Folder type="Child" label="Mobile" />
-            </div>
+            <h2>Coming Soon</h2>
         </div>
     )
 }
--- client/pages/kitchen.tsx
+++ client/pages/kitchen.tsx
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { FunctionComponent, useState, useContext, useEffect } from "react";
 
-const KitchenSink: React.FC = (): JSX.Element => {
+import styles from "../styles/index.module.css";
+
+import User from "../components/User";
+import ColorToggle from "../components/colorToggle";
+import Tag from "../components/Tag";
+import CopyLink from "../components/CopyLink";
+import Folder from "../components/Folder";
+
+const KitchenSinkPage: FunctionComponent = (): JSX.Element => {
+
+    const [name, setName] = useState<string>("");
+    const handleChange: Function = (event, handler) => {
+        handler(event.target.value);
+    }
+
     return (
         <div>
-            <h2>Hello World</h2>
-            <h2>LinksBook v2 Components Kicthen Sink</h2>
+
+            <div>
+                Profile
+                <User profile="/josias.jpg" name="Josias" />
+            </div>
+            <div>
+                Tag
+                <Tag name="Hello" />
+            </div>
+
+            <div>
+                Copy Link
+                <CopyLink link="https://hello.world" />
+            </div>
+
+            <div>
+                Folders
+                ------
+                Parent Type Folder
+                <Folder type="Parent" label="Mobile" />
+                Child Type Folder
+                <Folder type="Child" label="Mobile" />
+            </div>
         </div>
     )
-};
+}
 
-export default KitchenSink;
+export default KitchenSinkPage;
