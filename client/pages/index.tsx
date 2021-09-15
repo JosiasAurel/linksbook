d5d09eb9d211e2fdda68df2de033d0f6789aec74
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-15 20:33:17 +0100

    test client

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,22 @@
 import React, { FunctionComponent, useState, useContext, useEffect } from "react";
 
+import { useQuery, gql } from "@apollo/client";
+
 import Header from "../components/Header";
 import Search from "../components/Search";
 
 import styles from "../styles/index.module.css";
 
 const HomePage: FunctionComponent = (): JSX.Element => {
+    const Hello = gql`
+    query {
+        hello
+    }
+    `;
 
+    const { loading, error, data } = useQuery(Hello);
+
+    console.log(data);
     return (
         <div className={styles.dashboardPage}>
             <Header />
