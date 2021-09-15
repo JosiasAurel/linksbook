d0c715d4feaefb7a58296d74c7ae13f2b78ed6e7
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-15 20:06:41 +0100

    remove clerkjs

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -4,6 +4,9 @@ import "../styles/global.css";
 
 import { AppProps } from "next/app";
 
+/* Init Apollo Client */
+import { } from "@apollo/client";
+
 const LinksBookApp: FunctionComponent<AppProps> = ({ Component, pageProps }): JSX.Element => {
 
     return (
