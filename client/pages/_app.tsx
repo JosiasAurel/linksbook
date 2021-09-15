80555a36c8b4cb6c75a42884a28534fca345669c
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-15 20:33:04 +0100

    create apollo graphql client(with auth) and connect to backend

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -5,11 +5,29 @@ import "../styles/global.css";
 import { AppProps } from "next/app";
 
 /* Init Apollo Client */
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+    uri: "http://localhost:5000/graphql"
+});
+
+const authLink = setContext((_, { headers }) => {
+    const authToken = localStorage.getItem("token");
+
+    // headers included in request
+    return {
+        headers: {
+            ...headers,
+            authorization: authToken ? `Bearer ${authToken}` : ""
+        }
+    }
+});
 
 const client = new ApolloClient({
     uri: "http://localhost:5000/graphql",
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
+    link: authLink.concat(httpLink)
 });
 
 const LinksBookApp: FunctionComponent<AppProps> = ({ Component, pageProps }): JSX.Element => {
