24754c633a3ed91fd4448b1d2912d791f37a34a3
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-25 19:28:43 +0100

    add graphql actions

---
+++ client/graphql/actions.ts
@@ -0,0 +1,28 @@
+
+import { gql } from "@apollo/client";
+
+const FETCH_ALL = gql`
+    query {
+        user {
+            name, 
+            email,
+            links {
+                annotation,
+                tags,
+                note,
+                id,
+                url
+            }
+        }
+    }
+    `;
+
+const CREATE_LINK = gql`
+    mutation createLink($annotation: String!, $url: String!, $tags: String!) {
+        createLink(annotation: $annotation, url: $url, tags: $tags) {
+            status
+        }
+    }
+`;
+
+export { FETCH_ALL, CREATE_LINK };
