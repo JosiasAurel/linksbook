2dea874eec267461430840e0f913536aad2c377e
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-26 06:34:12 +0100

    add logo

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 import styles from "../styles/components.module.css";
 
-
-import { Avatar, Spacer } from "@nextui-org/react";
+import Image from "next/image";
+import { Avatar } from "@nextui-org/react";
 
 const Header: React.FC = (): JSX.Element => {
     return (
         <header className={styles.header}>
-            <div>
-                <h1>Star</h1>
+            <div className={styles.headerLogo}>
+                <Image src="/LinksBook.svg" width={40} height={40} />
             </div>
             <div>
                 <Avatar text={"Josias"} />
