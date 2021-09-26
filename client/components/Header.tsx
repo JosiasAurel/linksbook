81836b1747e92355884d7baed7a6154395d94583
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-26 07:12:24 +0100

    feat -> header consumes username from authcontext

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -5,14 +5,19 @@ import styles from "../styles/components.module.css";
 import Image from "next/image";
 import { Avatar } from "@nextui-org/react";
 
+import { AuthCtx } from "../contexts/auth";
+
 const Header: React.FC = (): JSX.Element => {
+
+    const gCtx = React.useContext(AuthCtx);
+
     return (
         <header className={styles.header}>
             <div className={styles.headerLogo}>
                 <Image src="/LinksBook.svg" width={40} height={40} />
             </div>
             <div>
-                <Avatar text={"Josias"} />
+                <Avatar text={gCtx.name} />
             </div>
         </header>
     )
