878c7676421d6cc9634d1d1f83f92975341f2545
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-26 13:57:28 +0100

    add login handler

--- client/pages/auth/login.tsx
+++ client/pages/auth/login.tsx
@@ -6,25 +6,52 @@ import Image from "next/image";
 import Link from "next/link";
 
 import styles from "../../styles/auth.module.css";
+import toast from "react-hot-toast";
+import router from "next/router";
+import { handleChange } from "../../utils/string";
+
+const AUTH_SERVICE_URI: string = process.env.NEXT_PUBLIC_AUTH_SERVICE;
 
 const LogInPage: React.FC = (): JSX.Element => {
 
-    const [name, setName] = React.useState<string>("");
     const [email, setEmail] = React.useState<string>("");
 
-    function handleChange(event, handler): void {
-        handler(event.target.value);
+    async function handleLogIn(): Promise<void> {
+        const response = await fetch(`${AUTH_SERVICE_URI}/create-login`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email })
+        });
+        const result = await response.json();
+
+        if (result.status === "Success") {
+            toast.success("Success");
+            router.replace("/auth/finish");
+        } else {
+            toast.error("Something Wrong Occurred");
+            toast("Please Try Again...");
+        }
+    }
+
+    async function logInUser(event: any): Promise<void> {
+        event.preventDefault(); // prevent broswer reload
+
+        if (email.trim() !== "") {
+            await handleLogIn();
+        } else {
+            toast.error("Please fill in your email");
+        }
     }
 
     return (
         <div className={styles.authPage}>
             <div className={styles.content}>
                 <Image src="/LinksBook.svg" width={150} height={150} />
-                <form>
+                <form onSubmit={e => logInUser(e)}>
                     <Input value={email} onChange={e => handleChange(e, setEmail)} clearable placeholder="kylee@acme.com">
                         Email
                     </Input>
-                    <Button>
+                    <Button htmlType="submit">
                         Log In
                     </Button>
                 </form>
