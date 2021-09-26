fe688ae1aa8d5ced948b1ae0713ad9ae4b725f9d
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-26 07:03:55 +0100

    create authentication context

--- client/contexts/auth.ts
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-
-const AuthCtx = createContext({});
---
+++ client/contexts/auth.tsx
@@ -0,0 +1,35 @@
+
+import React, { createContext } from "react";
+
+interface AuthCtxProps {
+    children: React.ReactChildren
+}
+
+const AuthProvider: React.FC<AuthCtxProps> = ({ children }): JSX.Element => {
+
+    const [isAuth, setIsAuth] = React.useState<boolean>(false);
+    const [name, setName] = React.useState<string>("");
+
+    React.useEffect(() => {
+        /* Request... Check if user is authenticated */
+        /* Is token in localstorage */
+        const authToken = localStorage.getItem("token") ?? undefined;
+        const userName = localStorage.getItem("name") ?? undefined;
+
+        if (authToken !== undefined && userName !== undefined) {
+            setIsAuth(true);
+            setName(userName);
+        }
+    }, []);
+    const AuthCtx = createContext({ authenticated: isAuth, name });
+
+    return (
+        <div>
+            <AuthCtx.Provider value={{ authenticated: isAuth, name: name }}>
+                {children}
+            </AuthCtx.Provider>
+        </div>
+    )
+}
+
+export default AuthProvider;
