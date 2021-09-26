3b98afda4b2b29a36d167263a5f4a672c34afd99
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-26 05:08:28 +0100

    add CREATE_LINK mutation exec

--- client/components/createLink.tsx
+++ client/components/createLink.tsx
@@ -2,10 +2,25 @@ import React from "react";
 
 import { Input, Button } from "@geist-ui/react";
 
+import { CREATE_LINK } from "../graphql/actions";
+import { useMutation } from "@apollo/client";
+
 const CreateLinkForm: React.FC = (): JSX.Element => {
+
+    const [url, setUrl] = React.useState<string>("");
+    const [button, setButton] = React.useState<any>();
+
+    // the create link mutation handler
+    const [saveLink, { data, loading, error }] = useMutation(CREATE_LINK);
+    async function handleCreateLink(event: any): Promise<void> {
+        event.preventDefault(); // prevent page reload
+
+        saveLink({ variables: { url, annotation: url, tags: [] } });
+    }
+
     return (
         <form style={{ display: "flex" }}>
-            <Input label="url" placeholder="https://twitter.com" />
+            <Input value={url} onChange={e => setUrl(e.target.value)} label="url" placeholder="https://twitter.com" />
             <Button style={{ margin: "0 5px" }} auto scale={0.8} type="success">Save</Button>
         </form>
     )
