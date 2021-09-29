e5eaaf483d7d40d69eab439b572e5ac753155ab3
JosiasAurel <ndjosiasaurel@gmail.com>
2021-09-29 04:37:22 +0100

    create notes card

---
+++ client/components/Note.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import styles from "../styles/components.module.css";
+
+interface NoteProps {
+    note: string
+}
+
+const Note: React.FC<NoteProps> = ({ note }): JSX.Element => {
+    return (
+        <div className={styles.noteCard}>
+            {note}
+        </div>
+    )
+}
+
+export default Note;
