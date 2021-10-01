09cba1f91a85e8f533e8381d5446e0b915d6b39a
JosiasAurel <ndjosiasaurel@gmail.com>
2021-10-01 01:28:59 +0100

    pass collection vars to gql mutation

--- client/components/createCollection.tsx
+++ client/components/createCollection.tsx
@@ -8,20 +8,20 @@ import toast from "react-hot-toast";
 
 const CreateCollectionForm: React.FC = (): JSX.Element => {
 
-    const [url, setUrl] = React.useState<string>("");
+    const [name, setName] = React.useState<string>("");
 
     // the create link mutation handler
     const [saveCollection, { data, loading, error }] = useMutation(CREATE_COLLECTION);
     async function handleCreateLink(event: any): Promise<void> {
         event.preventDefault(); // prevent page reload
 
-        toast.promise(saveCollection({ variables: { url, annotation: url, tags: [] } }), { loading: "Saving", success: "Bookmark saved", error: "Could not save bookmark" });
+        toast.promise(saveCollection({ variables: { name, type: "Parent" } }), { loading: "Saving", success: "Bookmark saved", error: "Could not save bookmark" });
 
     }
 
     return (
         <form onSubmit={e => handleCreateLink(e)} style={{ display: "flex" }}>
-            <Input value={url} onChange={e => setUrl(e.target.value)} label="Name" placeholder="Cat Photos" />
+            <Input value={name} onChange={e => setName(e.target.value)} label="Name" placeholder="Cat Photos" />
             <Button htmlType="submit" style={{ margin: "0 5px" }} auto scale={0.8} type="success">Save</Button>
         </form>
     )
