68488851f493b9e698dbbaa376be713709ca4e8c
JosiasAurel <ndjosiasaurel@gmail.com>
2021-10-09 11:04:29 +0100

    add mock routes for extension requests

--- server/index.ts
+++ server/index.ts
@@ -24,21 +24,43 @@ app.get("/", (req: Request, res: Response) => {
     res.send("LinksBook server working");
 });
 
+/* Routes to handle operations by the browser extension */
+app.post("/save-link", (req: Request, res: Response) => {
+
+    /* Requirements include which user ? */
+
+    const { name, email, id: key } = getUserInfo(req);
+
+    /*
+    --- Steps ---
+    Check if links with same URL exist
+    if yes ? respond with "existing" message link : otherwise 
+    create the link and responed with success message 
+    */
+
+});
+/* Routes for browser extension */
+
+function getUserInfo(req: Request): any {
+    const requestHeaders: any = req.headers;
+
+    const authorization: any = requestHeaders?.authorization;
+
+    // get the JWT from the request headers
+    const authToken: string = authorization?.split(" ")[1]; 
+
+    const userInfo = authenticateUser(authToken);
+
+    return userInfo;
+}
+
 // Create apollo server
 const apolloServer = new ApolloServer({
     typeDefs: typeDefinitions,
     resolvers,
     context: ({ req }) => {
         // handle request context
-
-        const requestHeaders: any = req.headers;
-
-        const authorization: any = requestHeaders?.authorization;
-
-        // get the JWT from the request headers
-        const authToken: string = authorization?.split(" ")[1]; 
-
-        const userInfo = authenticateUser(authToken);
+        const userInfo = getUserInfo(req);
 
         return userInfo;
     }
