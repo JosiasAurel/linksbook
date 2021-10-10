acaa9a0d7f8fb1b21554123976dd126a794db833
JosiasAurel <ndjosiasaurel@gmail.com>
2021-10-10 11:54:36 +0100

    remove bookmarks parser

--- extension/main.js
+++ extension/main.js
@@ -21,32 +21,3 @@ function handleBookmarks(bookmarks) {
     log(everything);
     log(bookmarksStructures);
 }
-
-let everything = [];
-
-function parseBookmarks(data, parent = "NONE") {
-
-    for (item in data) {
-        if (data[item].hasOwnProperty("children")) {
-            // folders with same name exists ?
-            // yes -> get the folder ID
-            // no --proceed
-            // create the folder
-            // if parent is NONE, pass...
-            // if parent is not NONE, modify parent to include this folder ID
-            // pass the ID of the new folder in the recursive call
-            // parseBookmarks(data[item].children, parentId)
-            // modify parent to include the ID of the new folder if this is child of parent
-            everything.push({folder: data[item].title, parent});
-            parseBookmarks(data[item].children, data[item].title);
-        } else {
-            // check if link (url) exists ?
-            // yes then skip
-            //  -> 
-            // no -> proceed to create
-            // create link
-            // modify parent to include id of new link
-            everything.push({bookmark: data[item].title, parent});
-        }
-    }
-}
