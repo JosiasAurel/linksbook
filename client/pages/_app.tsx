bfa12f5be66ab108b6422289352a9950b04cfd0f
JosiasAurel <ndjosiasaurel@gmail.com>
2021-10-13 02:32:03 +0100

    get server URI from env var

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -13,7 +13,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@ap
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-    uri: "http://localhost:5000/graphql"
+    uri: process.env.NEXT_PUBLIC_SERVER_URI
 });
 
 const authLink = setContext((_, { headers }) => {
