a18cc98499d1b79707e00681b190e0f1903a62c0
JosiasAurel <ndjosiasaurel@gmail.com>
2021-10-14 02:55:54 +0100

    create request handler

--- extAuth/main.js
+++ extAuth/main.js
@@ -1,5 +1,16 @@
 
-function init() 
-{
-    return;
+const AUTH_SERVICE_URI = "";
+
+async function makeRequest(route) {
+    let response = await fetch(`${AUTH_SERVICE_URI}/${route}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    });
+
+    let data = await response.json();
+
+    return data;
 }
