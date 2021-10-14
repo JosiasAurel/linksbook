5c31f8570d652fd368b5ef7fda01fa4cfafe4ef9
JosiasAurel <ndjosiasaurel@gmail.com>
2021-10-14 05:03:39 +0100

    ignore node_modules

---
+++ extAuth/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("client"));
+
+app.get("/", (req, res) => {
+    res.sendFile("./client/index.html");
+});
+
+app.post("/red", (req, res) => {
+    const { token } = req.body;
+    res.redirect(`/auth#token=${token}`);
+});
+
+app.get("/:any", (req, res) => {
+    res.send("Fine...");
+});
+
+module.exports = app;
+
+app.listen(7000, () => console.log("Working..."));
