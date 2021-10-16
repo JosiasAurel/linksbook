93e66a2fbffbff233c2661de73b542323fedb7c6
JosiasAurel <ndjosiasaurel@gmail.com>
2021-10-16 10:13:05 +0100

    show client dir content

--- extAuth/index.js
+++ extAuth/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static("client"));
 
 app.get("/", (req, res) => {
-    console.log(__dirname)
+    console.log(`${__dirname}/client`)
     res.sendFile(__dirname, "/client/index.html");
 });
 
