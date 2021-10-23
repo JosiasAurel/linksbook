5030b2890e043c1d9f606c02f9790f4052ee60c4
JosiasAurel <ndjosiasaurel@gmail.com>
2021-10-23 17:48:55 +0100

    return page url if no page title

--- api/app.py
+++ api/app.py
@@ -1,9 +1,19 @@
 
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from bs4 import BeautifulSoup
 import requests
 
 app = FastAPI()
+
+# add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -16,4 +26,7 @@ def _get_page_title(req: Request, url: str) -> any:
     page = requests.get(url).content
     soup = BeautifulSoup(page, "html.parser")
     title = soup.title.string
+
+    if title.strip() == "":
+        return url
     return {"pageTitle": title}
