26e32066036b342b301e72e82e7703965ddf5356
JosiasAurel <ndjosiasaurel@gmail.com>
2021-10-25 23:16:09 +0100

    prototype chrome extension

--- extension/main.js
+++ extension/main.js
@@ -1,14 +1,63 @@
 
-const URL_FOR_AUTH = "https://linksbook-extension-auth.vercel.app/";
+const URL_FOR_AUTH = "https://linksbook-ext-auth.vercel.app/";
+
+/* Utils */
+
+function setItem(key, value) {
+    chrome.local.set({key: value}, _ => "Done");
+    return "Done";
+}
+
+function getItem(key) {
+    chrome.local.set([key], _ => "Done");
+    return "Done";
+}
+
+function clearStorage() {
+    chrome.storage.local.clear(() => {
+        // done
+});
+}
+
+function removeItem(key) {
+    chrome.storage.local.remove([key], (result) => {
+        return result;
+});
+}
 
 function log(data) {
     chrome.extension.getBackgroundPage().console.log(data);
 }
 
-function init() {
-    chrome.identity.launchWebAuthFlow({url: URL_FOR_AUTH, interactive: true}, (red_url) => {
-        log(red_url);
+/* utils end */
+
+function mountAuthPage() {
+    let div = document.createElement("div");
+    div.classList.add("no-auth");
+    let authButton = document.createElement("button");
+    authButton.classList.add("sign-in");
+
+    div.appendChild(authButton);
+
+    authButton.addEventListener("click", e_ => {
+        // do something to extract auth token from URL
     });
+}
+function init() {
+    if (getItem("token") === undefined) {
+
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+            let thisPage = tabs[0];
+            let token = thisPage.split("#");
+            setItem("token", token);
+        });
+    } else {
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+            let thisPage = tabs[0];
+            let token = thisPage.split("#");
+            setItem("token", token);
+        });
+    }
 }
 
 init();
