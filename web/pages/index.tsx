0feabdad2db7e7a8a3616b005c2327b12ebb7cce
JosiasAurel <ndjosiasaurel@gmail.com>
2021-10-25 05:39:14 +0100

    add home page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 
+import Image from "next/image";
+
+import styles from "../styles/index.module.css";
+
 const HomePage: React.FC = (): JSX.Element => {
     return (
-        <div>
-            <h1>Hello World</h1>
+        <div className={styles.page}>
+            <header className={styles.header}>
+                <Image width="70" height="70" src="/LinksBook.svg" />
+                <h1>LinksBook</h1>
+            </header>
+
+            <main className={styles.content}>
+                <div>
+
+                </div>
+                <img className={styles.heroPage} src="/heroPage.png" alt="heroPage" />
+            </main>
         </div>
     )
 }
