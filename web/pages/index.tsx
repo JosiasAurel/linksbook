26db4aa004a91fe39bfd0148cfd674312d22ce38
JosiasAurel <ndjosiasaurel@gmail.com>
2021-10-28 19:35:52 +0100

    create homepage

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
-import Image from "next/image";
+import NImage from "next/image";
+import Link from "next/link";
+
+import { Image } from "@geist-ui/react";
 
 import styles from "../styles/index.module.css";
 
@@ -8,15 +11,32 @@ const HomePage: React.FC = (): JSX.Element => {
     return (
         <div className={styles.page}>
             <header className={styles.header}>
-                <Image width="70" height="70" src="/LinksBook.svg" />
-                <h1>LinksBook</h1>
+                <div>
+                    <NImage width="70" height="70" src="/LinksBook.svg" />
+                    <h1>LinksBook</h1>
+                </div>
+                <div className={styles.navLinks}>
+                    <Link href="/features">
+                        <p>Features</p>
+                    </Link>
+                    <Link href="/pricing">
+                        <p>Pricing</p>
+                    </Link>
+                    <Link href="/about">
+                        <p>About</p>
+                    </Link>
+                </div>
             </header>
 
             <main className={styles.content}>
-                <div>
-
+                <div className={styles.announcement}>
+                    <h1>
+                        Launching Soon 🙌
+                    </h1>
                 </div>
-                <img className={styles.heroPage} src="/heroPage.png" alt="heroPage" />
+                <Image.Browser url="app.linksbook.me">
+                    <Image width="50vw" height="60vh" src="/heroPage.png" />
+                </Image.Browser>
             </main>
         </div>
     )
