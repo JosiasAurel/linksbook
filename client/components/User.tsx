87332831855257be92b2f15d44212a339196e652
JosiasAurel <ndjosiasaurel@gmail.com>
2021-10-30 21:34:35 +0100

    add logout functionality

--- client/components/User.tsx
+++ client/components/User.tsx
@@ -5,6 +5,8 @@ import styles from "../styles/components.module.css";
 import ColorToggle from "./colorToggle";
 import { LogOut, Settings } from "@geist-ui/react-icons";
 
+const AUTH_SERVICE: string = process.env.NEXT_PUBLIC_AUTH_SERVICE;
+
 interface UserButtonProps {
     name: string
     toggleSettings?: Function
@@ -13,6 +15,14 @@ interface UserButtonProps {
 const User: React.FC<UserButtonProps> = ({ name, toggleSettings }): JSX.Element => {
 
     async function logOut(): Promise<any> {
+        await fetch(`${AUTH_SERVICE}/sign-out`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("token")}`
+            },
+            body: JSON.stringify({ action: "LogOut" })
+        });
 
     }
     return (
@@ -24,7 +34,7 @@ const User: React.FC<UserButtonProps> = ({ name, toggleSettings }): JSX.Element 
                 <div onClick={e_ => toggleSettings()}>
                     <Settings />
                 </div>
-                <div>
+                <div onClick={_ => logOut()}>
                     <LogOut color="red" />
                 </div>
             </div>
