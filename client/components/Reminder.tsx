cc2814a608d786f137a309f7d92b5de6e3a394b9
JosiasAurel <ndjosiasaurel@gmail.com>
2021-11-03 21:08:08 +0100

    create update reminder component

--- client/components/Reminder.tsx
+++ client/components/Reminder.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 
-import { Card, Badge, Tooltip, Button, Spacer, Divider } from "@geist-ui/react";
+import {
+    Card,
+    Badge,
+    Tooltip,
+    Button,
+    Spacer,
+    Divider,
+    Modal,
+    Textarea
+} from "@geist-ui/react";
 
-import { FETCH_ALL, DELETE_REMINDER } from "../graphql/actions";
+import { FETCH_ALL, DELETE_REMINDER, UPDATE_REMINDER } from "../graphql/actions";
+import { handleChange } from "../utils/string";
 import { useMutation } from "@apollo/client";
 import toast from "react-hot-toast";
+
+import DateTimePicker from "react-datetime-picker/dist/entry.nostyle";
+import "react-datetime-picker/dist/DateTimePicker.css";
+import "react-calendar/dist/Calendar.css";
+import "react-clock/dist/Clock.css";
 
 interface ReminderProps {
     recipients: Array<string>
@@ -12,9 +27,59 @@ interface ReminderProps {
     id: string
     getUpdatedData?: Function
     linkId: string
+    data: any
 }
 
-function ReminderTooltipBody({ date, recipients, reminder, getUpdatedData, linkId }): JSX.Element {
+function UpdateReminderComponent({ getUpdatedData, finishAction, reminder, oldData }): JSX.Element {
+
+    const [updateReminder, { data, loading, error }] = useMutation(UPDATE_REMINDER);
+
+    // set reminder form
+    const [remindDate, setRemindDate] = React.useState(new Date(oldData.remindDate));
+
+    function prepareUTCRemindDate(): string {
+        const gmtTime = remindDate.toUTCString();
+
+        return gmtTime;
+    }
+
+    const [recipients, setRecipients] = React.useState<Array<string>>(oldData.recipients);
+    function recipientsHandler(v: any): void {
+        setRecipients(v.split(" "));
+    }
+
+    function handleUpdateReminder(): void {
+        toast.promise(updateReminder({ variables: { reminderId: reminder, remindDate: prepareUTCRemindDate(), recipients: recipients.join(" ").trim().split(" ") }, refetchQueries: [{ query: FETCH_ALL }] }), {
+            success: "Reminder Updated",
+            error: "Something went wrong",
+            loading: "Updating Reminder..."
+        }).then(_ => getUpdatedData(data));
+
+        finishAction(false);
+    }
+
+    return (
+        <div>
+            <h2>Update Reminder</h2>
+            <div>
+                <DateTimePicker
+                    value={remindDate}
+                    onChange={setRemindDate}
+                />
+                <Spacer />
+                <Textarea value={recipients.join(" ")} onChange={e => handleChange(e, recipientsHandler)} width="100%" h="100px" placeholder="Enter receivers email separated by spaces. Leave blank so you're email is used" />
+                <Spacer />
+                <Button onClick={e_ => handleUpdateReminder()}>
+                    Save Reminder
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+function ReminderTooltipBody({ data_, date, recipients, reminder, getUpdatedData, linkId }): JSX.Element {
+
+    const [updateModal, setUpdateModal] = React.useState<boolean>(false);
 
     const [deleteReminder, { data, loading, error }] = useMutation(DELETE_REMINDER);
 
@@ -44,7 +109,7 @@ function ReminderTooltipBody({ date, recipients, reminder, getUpdatedData, linkI
             <Divider />
 
             <div style={{ display: "flex", flexDirection: "row" }}>
-                <Button ghost type="success">
+                <Button ghost type="success" onClick={_ => setUpdateModal(!updateModal)}>
                     Update
                 </Button>
                 <Spacer />
@@ -52,13 +117,27 @@ function ReminderTooltipBody({ date, recipients, reminder, getUpdatedData, linkI
                     Delete
                 </Button>
             </div>
+
+            <Modal visible={updateModal} onClose={() => setUpdateModal(false)}>
+                <Modal.Title>
+                    Update Reminder
+                </Modal.Title>
+                <Modal.Content>
+                    <UpdateReminderComponent
+                        getUpdatedData={d => getUpdatedData(d)}
+                        finishAction={v => setUpdateModal(v)}
+                        reminder={reminder}
+                        oldData={data_}
+                    />
+                </Modal.Content>
+            </Modal>
         </>
     )
 }
 
-const Reminder: React.FC<ReminderProps> = ({ recipients, date, id, getUpdatedData, linkId }): JSX.Element => {
+const Reminder: React.FC<ReminderProps> = ({ data, recipients, date, id, getUpdatedData, linkId }): JSX.Element => {
     return (
-        <Tooltip text={<ReminderTooltipBody linkId={linkId} getUpdatedData={d => getUpdatedData(d)} reminder={id} recipients={recipients} date={date} />} placement="bottom">
+        <Tooltip text={<ReminderTooltipBody data_={data} linkId={linkId} getUpdatedData={d => getUpdatedData(d)} reminder={id} recipients={recipients} date={date} />} placement="left">
             <Card shadow>
                 <Badge>
                     {recipients.length}
@@ -67,5 +146,7 @@ const Reminder: React.FC<ReminderProps> = ({ recipients, date, id, getUpdatedDat
         </Tooltip>
     )
 }
+
+
 
 export default Reminder;
