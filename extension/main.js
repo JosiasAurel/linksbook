af251ac3a6ec627d8b6bf59c87c830abcbf9185b
JosiasAurel <ndjosiasaurel@gmail.com>
2021-11-16 20:51:49 +0100

    prettify code

--- extension/main.js
+++ extension/main.js
@@ -1,74 +1,71 @@
-
 const app = document.getElementById("app");
 
-const SERVER_URI = "http://localhost:5000" //"https://server.linksbook.me";
+const SERVER_URI = "http://localhost:5000"; //"https://server.linksbook.me";
 
 /* Utils */
 
 function log(data) {
-    chrome.extension.getBackgroundPage().console.log(data);
+  chrome.extension.getBackgroundPage().console.log(data);
 }
 
 /* utils end */
 function showAppPage(pageTitle, pageUrl) {
-    /* Create basic elements */
-    
-    const syncButton = document.createElement("button");
-    syncButton.innerText = "Sync Browser";
-    syncButton.setAttribute("id", "synclinks");
-    syncButton.classList.add("button");
+  /* Create basic elements */
 
-    const titleInput = document.createElement("input");
-    titleInput.type = "text";
-    titleInput.setAttribute("placeholder", "Annotation");
-    titleInput.value = pageTitle;
-    titleInput.setAttribute("id", "title-input");
+  const syncButton = document.createElement("button");
+  syncButton.innerText = "Sync Browser";
+  syncButton.setAttribute("id", "synclinks");
+  syncButton.classList.add("button");
 
-    const urlInput = document.createElement("input");
-    urlInput.type = "url";
-    urlInput.setAttribute("placeholder", "URL");
-    urlInput.value = pageUrl;
-    urlInput.setAttribute("id", "url-input");
+  const titleInput = document.createElement("input");
+  titleInput.type = "text";
+  titleInput.setAttribute("placeholder", "Annotation");
+  titleInput.value = pageTitle;
+  titleInput.setAttribute("id", "title-input");
 
-    const saveButton = document.createElement("button");
-    saveButton.innerText = "Save";
-    saveButton.type = "submit";
-    saveButton.setAttribute("id", "saveBtn");
-    saveButton.classList.add("button");
+  const urlInput = document.createElement("input");
+  urlInput.type = "url";
+  urlInput.setAttribute("placeholder", "URL");
+  urlInput.value = pageUrl;
+  urlInput.setAttribute("id", "url-input");
 
-    /* Set input field values */
+  const saveButton = document.createElement("button");
+  saveButton.innerText = "Save";
+  saveButton.type = "submit";
+  saveButton.setAttribute("id", "saveBtn");
+  saveButton.classList.add("button");
 
-    /* Construct hierachy */
-    
-    const form = document.createElement("form");
-    form.appendChild(titleInput);
-    form.appendChild(urlInput);
-    form.appendChild(saveButton);
-    form.setAttribute("id", "create-link");
+  /* Set input field values */
 
-    const container = document.createElement("div");
-    container.classList.add("container");
-    container.appendChild(syncButton);
-    container.appendChild(form);
+  /* Construct hierachy */
 
-    return container;
+  const form = document.createElement("form");
+  form.appendChild(titleInput);
+  form.appendChild(urlInput);
+  form.appendChild(saveButton);
+  form.setAttribute("id", "create-link");
+
+  const container = document.createElement("div");
+  container.classList.add("container");
+  container.appendChild(syncButton);
+  container.appendChild(form);
+
+  return container;
 }
 
 function Setup() {
-    const userEmail = prompt("Enter your email address");
-    chrome.storage.local.set({email: userEmail});
+  const userEmail = prompt("Enter your email address");
+  chrome.storage.local.set({ email: userEmail });
 }
 
 function init() {
-
-    chrome.storage.local.get("email", (res) => {
-        console.log("init res");
-        console.log(res);
-        if (res.email === undefined) {
-            Setup();
-        }  else {
-
-    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+  chrome.storage.local.get("email", (res) => {
+    console.log("init res");
+    console.log(res);
+    if (res.email === undefined) {
+      Setup();
+    } else {
+      chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
         let thisPage = tabs[0];
         const url = thisPage.url;
         const title = thisPage.title;
@@ -80,83 +77,80 @@ function init() {
         /* wanna save the link ? */
         const saveLinkForm = document.getElementById("create-link");
 
-        saveLinkForm.addEventListener("submit", async  e => {
-            e.preventDefault(); // prevent reload
-            chrome.storage.local.get("email", async res => {
-                const titleInput = document.getElementById("title-input");
+        saveLinkForm.addEventListener("submit", async (e) => {
+          e.preventDefault(); // prevent reload
+          chrome.storage.local.get("email", async (res) => {
+            const titleInput = document.getElementById("title-input");
             const urlInput = document.getElementById("url-input");
 
             let url = urlInput.value;
             let annotation = titleInput.value;
             const response = await fetch(`${SERVER_URI}/save-link`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ email: res.email, annotation, url })
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ email: res.email, annotation, url }),
             });
 
             const result = await response.json();
 
             log(result);
-            });
+          });
 
-            /* Sync bookmarks */
-            const syncBookmarks = document.getElementById("synclinks");
-            syncBookmarks.addEventListener("click", async _ => {
-                chrome.bookmarks.getTree(async bkms => {
-                    await handleBookmarks(bkms);
-                });
+          /* Sync bookmarks */
+          const syncBookmarks = document.getElementById("synclinks");
+          syncBookmarks.addEventListener("click", async (_) => {
+            chrome.bookmarks.getTree(async (bkms) => {
+              await handleBookmarks(bkms);
             });
-        })
-            });
-    	}
-    })
+          });
+        });
+      });
+    }
+  });
 }
 init();
 
-
-chrome.bookmarks.getTree(bkms => {
-    handleBookmarks(bkms);
+chrome.bookmarks.getTree((bkms) => {
+  handleBookmarks(bkms);
 });
 
 var everything = [];
 
 async function handleBookmarks(bookmarks_) {
+  chrome.storage.local.get("email", async (res) => {
+    // Assuming all fetched bookmarks will always be Array(1)
+    // We get all bookmark and folders from the browser
+    const bookmarks = bookmarks_[0].children;
 
-    chrome.storage.local.get("email", async res => {
-        // Assuming all fetched bookmarks will always be Array(1)
-        // We get all bookmark and folders from the browser
-        const bookmarks = bookmarks_[0].children;
+    parseBookmarks(bookmarks);
+    log(everything);
 
-        parseBookmarks(bookmarks);
-        log(everything);
+    const response = await fetch(`${SERVER_URI}/sync-bookmarks`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: res.email, bookmarks }),
+    });
 
-        const response = await fetch(`${SERVER_URI}/sync-bookmarks`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ email: res.email, bookmarks })
-        });
+    const result = await response.json();
 
-        const result = await response.json();
-
-        log(result);
-        // log(bookmarksStructures);
-    })
-};
+    log(result);
+    // log(bookmarksStructures);
+  });
+}
 
 function parseBookmarks(data) {
-
-    for (item in data) {
-        if (data[item].hasOwnProperty("children")) {
-            everything.push({folder: data[item].title});
-            parseBookmarks(data[item].children);
-        } else {
-            everything.push({bookmark: data[item].title});
-        }
+  for (item in data) {
+    if (data[item].hasOwnProperty("children")) {
+      everything.push({ folder: data[item].title });
+      parseBookmarks(data[item].children);
+    } else {
+      everything.push({ bookmark: data[item].title });
     }
+  }
 }
 
 // LinksBook v2 Chrome Extension
--- extension/util.js
+++ extension/util.js
@@ -1,22 +1,21 @@
-
 function setItem(key, value) {
-    chrome.local.set({key: value}, _ => "Done");
-    return "Done";
+  chrome.local.set({ key: value }, (_) => "Done");
+  return "Done";
 }
 
 function getItem(key) {
-    chrome.local.set([key], _ => "Done");
-    return "Done";
+  chrome.local.set([key], (_) => "Done");
+  return "Done";
 }
 
 function clearStorage() {
-    chrome.storage.local.clear(() => {
-        // done
-});
+  chrome.storage.local.clear(() => {
+    // done
+  });
 }
 
 function removeItem(key) {
-    chrome.storage.local.remove([key], (result) => {
-        return result;
-});
+  chrome.storage.local.remove([key], (result) => {
+    return result;
+  });
 }
