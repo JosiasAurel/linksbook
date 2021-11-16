11d7b9cbe3c9d2aa4b2dd8b02d4f4860cd575e7a
JosiasAurel <ndjosiasaurel@gmail.com>
2021-11-16 23:08:18 +0100

    make public folder page

--- client/pages/p/[collectionid].tsx
+++ client/pages/p/[collectionid].tsx
@@ -2,13 +2,43 @@ import React from "react";
 import Head from "next/head";
 import { Deta } from "deta";
 
-const PublicCollection: React.FC = (): JSX.Element => {
+import styles from "../../styles/misc.module.css";
+
+import PublicLink from "../../components/PublicLink";
+
+interface PublicCollectionProps {
+    pageData: any
+}
+
+const PublicCollection: React.FC<PublicCollectionProps> = ({ pageData }): JSX.Element => {
+
     return (
-        <div>
+        <div className={styles.publicPage}>
             <Head>
-                <title>Hello World</title>
+                <title> {pageData.folder} </title>
+                <link rel="shortcut icon" href="LinksBook.svg" type="image/x-icon" />
             </Head>
-            <h1>Hello</h1>
+            <header className={styles.header}>
+                <h1> {pageData.folder} </h1>
+            </header>
+            <main className={styles.content}>
+                <div className={styles.bkmks}>
+                    {pageData.bookmarks.map((b: { url: string; title: string; }) => {
+                        return (
+                            <PublicLink
+                                key={b.url}
+                                name={b.title}
+                                url={b.url}
+                            />
+                        )
+                    })}
+                </div>
+            </main>
+            <footer style={{ textAlign: "center", margin: "1em 0" }}>
+                Made on <a href="https://linksbook.me">
+                    linksbook.me
+                </a>
+            </footer>
         </div>
     )
 }
